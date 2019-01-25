eeebde5b473585c1f6051645f9345d05b7fff256
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-01-24 21:52:01 -0600

    Created Project

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace CalorieCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            var subType = String.Empty;
+            var name = String.Empty;
+            var bread = String.Empty;
+            bool extra_cheese = false;
+            var category = String.Empty;
+
+            Console.WriteLine("Hello Welcome to the Subway!");
+
+            while (name == "")
+            {
+
+                Console.WriteLine($"Enter your Name: ");
+                name = Console.ReadLine();
+            }
+
+            while (subType == "")
+            {
+
+                Console.WriteLine($"Do you want Salad/Sandwich: ");
+                subType = Console.ReadLine();
+            }
+
+            if (subType.ToLower() == "sandwich")
+            {
+                while (category == "")
+                {
+                    Console.Write("Please enter subType of bread italian_herbs or garlic: ");
+                    category = Console.ReadLine();
+                }
+            }
+
+
+            Console.WriteLine($"Do you want extra_cheese(Y/N): ");
+           
+            var extraCheese = Console.ReadLine();
+            if (extraCheese.ToLower().Equals("y"))
+                extra_cheese = true;
+
+            Console.WriteLine("Hello {0} your total cost of your subType is: {1}", name, calTotalCost(category, extra_cheese));
+            Console.ReadLine();
+        }
+
+        private static decimal calTotalCost(String subType, bool extra_cheese)
+        {
+
+            var totalCost = 0.0m;
+            switch (subType.ToLower())
+            {
+
+                case "italian_herbs":
+                    totalCost = 2.99m;
+                    break;
+
+                case "garlic":
+                    totalCost = 1.99m;
+                    break;
+            }
+
+            if (extra_cheese)
+            {
+                totalCost += 1.00m;
+            }
+
+            return totalCost;
+        }
+    }
+}
